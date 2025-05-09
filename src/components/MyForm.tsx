087e2e351f6a77deb78f461d46cfa0504fe41910
Nils Urmersbach <nils.urmersbach@capgemini.com>
2025-05-09 11:37:42 +0200

    adding form validation

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,14 +1,19 @@
 import { useForm } from "react-hook-form";
 import { BookDetails } from "../API/useDetails";
 import { Book } from "./Book";
+import styled from "styled-components";
 
 interface props {
   books: Book[];
   setBooks: (books: BookDetails[]) => void;
 }
 
+const ErrorMessage = styled.div`
+  color: red;
+`;
+
 export const MyForm: React.FC<props> = ({books, setBooks}: props) => {
- const {register, handleSubmit} = useForm<BookDetails>({
+ const {register, handleSubmit, formState: {errors}} = useForm<BookDetails>({
     defaultValues:{
       title: "",
       subtitle: "",
@@ -18,14 +23,26 @@ export const MyForm: React.FC<props> = ({books, setBooks}: props) => {
   })
   const onSubmit = (data: BookDetails) => {
     console.log(data);
-    setBooks( [...books, {image: data.image, title: data.title, subtitle: data.subtitle }] )
+    setBooks( [{image: data.image, title: data.title, subtitle: data.subtitle }, ...books] )
   }
   
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("title")} placeholder="Title" /><br />
+      <input {...register("title", {required: true, minLength: 3, maxLength: 20})} placeholder="Title" /><br />
+      {errors.title?.type === "required" && <ErrorMessage>This field is required</ErrorMessage>}
+      {errors.title?.type === "minLength" && <ErrorMessage>Title must be at least 3 characters</ErrorMessage>}
+      {errors.title?.type === "maxLength" && <ErrorMessage>Title must be at most 20 characters</ErrorMessage>}
       <input {...register("subtitle")} placeholder="Subtitle" /><br />
-      <input {...register("authors")} placeholder="Authors" /><br />
+      <input {...register("authors", {
+        required: true,
+        pattern: {
+          value: /^[A-Za-z\s]+$/,
+          message: "Author should only consist of letters and spaces"
+          }
+        })
+      } placeholder="Authors" /><br />
+      {errors.authors?.type === "required" && <ErrorMessage>This field is required</ErrorMessage>}
+      {errors.authors?.type === "pattern" && <ErrorMessage>Author should only consist of letters and spaces</ErrorMessage>}
       <input {...register("publisher")} placeholder="Publisher" /><br />
       <button type="submit">Submit</button>
     </form>
